a056e8e3198a290c3dcd154b3af63acf5b282d5b
ackerwomannx <isabela.ackermann1@gmail.com>
2023-04-03 17:05:36 -0300

    func arrumada

--- src/functions.py
+++ src/functions.py
@@ -7,13 +7,12 @@ def medias(df:pd.DataFrame):
         media[i] = np.average(df.loc[:,df.columns[i]])
     return media
 
-def media_por_semana(df:pd.DataFrame):
-    """ a ultima semana de todas as pessoas ta saindo igual a zero, achar o erro"""
+def media_por_dia(df:pd.DataFrame):
 
-    range_semanas = np.array(range(0, len(df.axes[0])+1, 1440))
+    range_dias = np.array(range(0, len(df.axes[0])+1, 1440))
     media = np.zeros((len(df.axes[1]), 7))
 
     for i in range(0, len(df.axes[1])):
-        for j in range(0,6):
-            media[i,j] = np.average(df.loc[range(range_semanas[j], range_semanas[j+1]-1),df.columns[i]])
+        for j in range(0,7):
+            media[i,j] = np.average(df.loc[range(range_dias[j], range_dias[j+1]-1),df.columns[i]])
     return media
--- test.py
+++ test.py
@@ -10,16 +10,18 @@ df_data = df_data.drop(df_data.columns[0], axis=1)
 #print(df_data)
 #print(df_groups)
 
-def media_por_semana(df:pd.DataFrame):
+def media_por_dia(df:pd.DataFrame):
     """ a ultima semana de todas as pessoas ta saindo igual a zero, achar o erro"""
 
-    range_semanas = np.array(range(0, len(df.axes[0])+1, 1440))
+    range_dias = np.array(range(0, len(df.axes[0])+1, 1440))
+    print(range_dias)
+    
     media = np.zeros((len(df.axes[1]), 7))
 
     for i in range(0, len(df.axes[1])):
-        for j in range(0,6):
-            media[i,j] = np.average(df.loc[range(range_semanas[j], range_semanas[j+1]-1),df.columns[i]])
+        for j in range(0,7):
+            media[i,j] = np.average(df.loc[range(range_dias[j], range_dias[j+1]-1),df.columns[i]])
     return media
 
-print(media_por_semana(df_data))
+print(media_por_dia(df_data))
 
