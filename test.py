13bdfde57e16d10025f6201d501437f54bacb8f5
RicGary <ericnaiber@gmail.com>
2023-04-03 02:44:21 -0300

    limpeza inicial dos dados

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/timeseries_NEW.csv')
+df_data = pd.read_csv('data/timeseries_NEW.csv')
+df_groups = pd.read_csv('data/timeseries_classification.csv', index_col=0)
 
-print(df.shape)
+df_data = df_data.drop(df_data.columns[0], axis=1)
+
+print(df_data)
