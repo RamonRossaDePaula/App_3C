836d2de7af40174941e25476011cbc9e0fe09580
BelaAckermann <isabela.ackermann@poatek.com>
2023-04-03 16:04:19 -0300

    criei o file de funcoes

--- src/functions.py
+++ src/functions.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-df_data = pd.read_csv('data/timeseries_NEW.csv')
-df_groups = pd.read_csv('data/timeseries_classification.csv', index_col=0)
-
-df_data = df_data.drop(df_data.columns[0], axis=1)
-
-#print(df_data)
-#print(df_groups)
+def medias(df:pd.DataFrame):
+    media = np.zeros(len(df.axes[1]))
+    for i in range(len(df.axes[1])):
+        media[i] = np.average(df.loc[:,df.columns[i]])
+    return media
 
 def media_por_semana(df:pd.DataFrame):
     """ a ultima semana de todas as pessoas ta saindo igual a zero, achar o erro"""
@@ -20,6 +17,3 @@ def media_por_semana(df:pd.DataFrame):
         for j in range(0,6):
             media[i,j] = np.average(df.loc[range(range_semanas[j], range_semanas[j+1]-1),df.columns[i]])
     return media
-
-print(media_por_semana(df_data))
-
