ecb808c0db1cb1c3f234d03df30a3436d6bb337c
RamonRossaDePaula <ramonrossadepaula@hotmail.com>
2024-05-03 12:57:08 -0300

    Test 4

--- app/src/main/java/com/example/calculonaconstrucaocivil/MainActivity.kt
+++ app/src/main/java/com/example/calculonaconstrucaocivil/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculonaconstrucaocivil.ui.theme.CálculoNaConstruçãoCivilTheme
 
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
